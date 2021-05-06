dcf7f7e1c65745d6da75ffd069eb4313dd4c78af
Kusasaki <75423246+Kusasaki@users.noreply.github.com>
2021-05-06 15:22:03 +0200

    Maj 1 06/05

--- Test_Roguelike/Game.cs
+++ Test_Roguelike/Game.cs
@@ -16,7 +16,7 @@ namespace Test_Roguelike
     {
         // The screen height and width are in number of tiles
         private static readonly int _screenWidth = 150;
-        private static readonly int _screenHeight = 80;
+        private static readonly int _screenHeight = 90;
         public static RLRootConsole _rootConsole { get; private set; }
 
         // The map console takes up most of the screen and is where the map will be drawn
@@ -25,18 +25,18 @@ namespace Test_Roguelike
         private static RLConsole _mapConsole;
 
         // Below the map console is the message console which displays attack rolls and other information
-        private static readonly int _messageWidth = 150;
+        private static readonly int _messageWidth = 120;
         private static readonly int _messageHeight = 15;
         private static RLConsole _messageConsole;
 
         // The stat console is to the right of the map and display player and monster stats
         private static readonly int _statWidth = 30;
-        private static readonly int _statHeight = 65;
+        private static readonly int _statHeight = 90;
         private static RLConsole _statConsole;
 
         // Above the map is the inventory console which shows the players equipment, abilities, and items
         private static readonly int _inventoryWidth =  120;
-        private static readonly int _inventoryHeight = 5;
+        private static readonly int _inventoryHeight = 15;
         private static RLConsole _inventoryConsole;
         
         public static DungeonMap DungeonMap { get; private set; }
@@ -90,19 +90,6 @@ namespace Test_Roguelike
             DungeonMap.UpdatePlayerFieldOfView();
 
             CommandSystem = new CommandSystem();
-
-            // Set background color and text for each console so that we can verify they are in the correct positions
-            _mapConsole.SetBackColor(0, 0, _mapWidth, _mapHeight, Colors.FloorBackground);
-            _mapConsole.Print(1, 1, "Map", Colors.TextHeading);
-
-            _messageConsole.SetBackColor(0, 0, _messageWidth, _messageHeight, RLColor.Gray);
-            _messageConsole.Print(1, 1, "Messages", Colors.TextHeading);
-
-            _statConsole.SetBackColor(0, 0, _statWidth, _statHeight, Swatch.Primary);
-            _statConsole.Print(1, 1, "Stats", Colors.TextHeading);
-
-            _inventoryConsole.SetBackColor(0, 0, _inventoryWidth, _inventoryHeight, Swatch.Primary);
-            _inventoryConsole.Print(1, 1, "Inventory", Colors.TextHeading);
 
             // Set up a handler for RLNET's Update event
             _rootConsole.Update += OnRootConsoleUpdate;
@@ -235,12 +222,15 @@ namespace Test_Roguelike
 
                 // Tell RLNET to draw the console that we set
                 _rootConsole.Draw();
+
                 MessageLog.Draw(_messageConsole);
                 InventoryLog.Draw(_inventoryConsole);
 
-                _inventoryConsole.SetBackColor(0, 0, _inventoryWidth, _inventoryHeight, Swatch.Primary);
-                _messageConsole.SetBackColor(0, 0, _messageWidth, _messageHeight, RLColor.Gray);
-                
+                _mapConsole.SetBackColor(0, 0, _mapWidth, _mapHeight, Swatch.ComplementDarker);
+                _messageConsole.SetBackColor(0, 0, _messageWidth, _messageHeight, Swatch.Primary2Darker);
+                _statConsole.SetBackColor(0, 0, _statWidth, _statHeight, Swatch.PrimaryLighter);
+                _inventoryConsole.SetBackColor(0, 0, _inventoryWidth, _inventoryHeight, Swatch.Primary2Darker);
+
                 _renderRequired = false;
             }
         }
